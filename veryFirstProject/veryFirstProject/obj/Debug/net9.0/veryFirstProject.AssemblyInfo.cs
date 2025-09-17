111a62b88361599b3cd10346736fc0f831b96efa
omarsauceydough <os0996762@otc.edu>
2025-09-16 23:04:22 -0500

    added readme

--- veryFirstProject/veryFirstProject/obj/Debug/net9.0/veryFirstProject.AssemblyInfo.cs
+++ veryFirstProject/veryFirstProject/obj/Debug/net9.0/veryFirstProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("veryFirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae4c62ecab199f48703e59b8394c1f781a9295fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("veryFirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("veryFirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
