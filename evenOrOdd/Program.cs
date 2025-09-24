e1314a7eafac8d04b3dafe2f62409b556fa6cd85
omarsauceydough <os0996762@otc.edu>
2025-09-23 22:39:34 -0500

    Initial commit for EvenorOdd

---
+++ evenOrOdd/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Greet user and ask for a number 
+        Console.WriteLine("Welcome to the Even or Odd number calculator! Please enter a number >> ");
+        int number = int.Parse(Console.ReadLine());
+
+        //switch for determining even or odd, which produces result 
+        switch (number % 2)
+        {
+            case 0:
+                Console.WriteLine(number + " is even!");
+                break;
+            case 1:
+                Console.WriteLine(number + " is odd!");
+                break;
+        }
+    }
+}
---
+++ veryFirstProject/Switches/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("1) english");
+Console.WriteLine("2) spanish");
+Console.WriteLine("3) ebonics");
+//blank space for "spacing"
+Console.WriteLine();
+
+// Takes in a llangauge choice
+Console.Writeline(">> ");
+string choice = Console.ReadLine();
+
+Console.WriteLine();
+
+Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+
+//empty string called greeting
+string greeting;
+
+// starts a swtich with chpoce as the cases
+switch (choice)
+{
+    case "english":
+        greeting = $"Hello, {name}!";
+        //prevents the swtich from falling through
+        break;
+    case "spanish":
+        greeting = $"Hola, {name}";
+        break;
+    case "ebonics":
+        greeting = $"Wuz good, {name}";
+        break;
+    default:
+        greeting = $"Invalid input, {name}";
+        break;
+}
+
+Console.WriteLine();
+
+Console.WriteLine(greeting);
