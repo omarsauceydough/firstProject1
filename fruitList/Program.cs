15563e7b96029276b776265ef21bfd293e522139
omarsauceydough <os0996762@otc.edu>
2025-10-08 16:57:31 -0500

    final commit

--- fruitList/Program.cs
+++ fruitList/Program.cs
@@ -1,1 +1,66 @@
-﻿//
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Create the list
+        List<string> Fruits = new List<string> { "Apples", "Oranges", "Strawberries", "Mangos", "Guavas" };
+
+        //Use forloop to print list
+        Console.WriteLine("List of food before any changes:");
+        foreach (string fruit in Fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+        Console.WriteLine("----------------------------------");
+
+        //Addition of 2 more fruits
+        Fruits.Add("Pineapples");
+        Fruits.Add("Grapes");
+
+        //Display the list after adding pineapples and grapes
+        Console.WriteLine("\nList after adding Pineapples and Grapes:");
+        foreach (string fruit in Fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+        Console.WriteLine("----------------------------------");
+
+        //Remove 1 (wan) fruit from the list 
+        Fruits.Remove("Oranges");
+
+        //Display list after removing oranges
+        Console.WriteLine("\nAfter removing Oranges:");
+        foreach (string fruit in Fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+        Console.WriteLine("----------------------------------");
+
+        //Three different methods (Contains, Sort, Reverse)
+        //Using contains method to test for item in a list
+        bool hasApples = Fruits.Contains("Apples");
+        Console.WriteLine($"\nDoes the list CONTAIN Apples?: {hasApples}");
+        Console.WriteLine("----------------------------------");
+
+        //Using sort method to sort items in alphabetical order
+        Fruits.Sort();
+        Console.WriteLine("\nList after reversing order: ");
+        foreach (string fruit in Fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+        Console.WriteLine("----------------------------------");
+
+        //Using reverse method to display the items in reverse
+        Fruits.Reverse();
+        Console.WriteLine("\nList in reverse order:");
+        foreach (string fruit in Fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+        Console.WriteLine("----------------------------------");
+
+        //Final message
+        Console.WriteLine("\nThere you have it! Thanks for running!");
+    }
+}
