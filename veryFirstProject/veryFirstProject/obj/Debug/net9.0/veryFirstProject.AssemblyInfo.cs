7f463b6672190a5fd7e3492b8d6c9f67984c4065
omarsauceydough <os0996762@otc.edu>
2025-12-03 15:52:26 -0600

    inital commit

--- veryFirstProject/veryFirstProject/obj/Debug/net9.0/veryFirstProject.AssemblyInfo.cs
+++ veryFirstProject/veryFirstProject/obj/Debug/net9.0/veryFirstProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("veryFirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+548aed8ae2109f460e29129ffedd68dda9e399ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74c34c393b726d6eeb39c4a182f76958cd4ee6e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("veryFirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("veryFirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
